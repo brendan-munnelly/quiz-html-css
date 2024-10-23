de4b67f92d481911348d0300b7debf7332e08640
Brendan Munnelly <munnellyb@gmail.com>
2024-10-23 02:40:27 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
-  base: '/quiz-html-css/',
+  base: '/quiz-dorset/',
 })
+echo "# quiz-dorset" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:brendan-munnelly/quiz-dorset.git
+git push -u origin main
