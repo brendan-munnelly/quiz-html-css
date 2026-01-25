d5407bf732543edcd33337f59e9269e61978c725
Brendan Munnelly <munnellyb@gmail.com>
2026-01-25 08:36:14 +0000

    CAPITALS added

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   plugins: [react()],
-<<<<<<< HEAD
   base: '/quiz-dorset-javascript/',
-=======
-  base: '/quiz-html-css/',
->>>>>>> 8a5250383156cf60488055e4f6fe29d6bb652a3e
 })
